8cd2cd64db79a4880b8323cbe063e47f9f967728
0xsmiler <anthonylee110.eng@gmail.com>
2025-04-13 09:00:12 -0400

    wagmi config

--- src/components/TradingView.jsx
+++ src/components/TradingView.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
 const TradingViewWidget = () => {
   return (
     <div className="w-full h-[500px]">
       <iframe
-        src="https://www.dextools.io/widget-chart/en/ether/0x086c01cd7891e8aed5fd27c01dcca6081b30318203aec3474c47464c030d9492?theme=dark"
+        src="https://www.dextools.io/widget-chart/en/base/pe-light/0x086c01cd7891e8aed5fd27c01dcca6081b30318203aec3474c47464c030d9492?theme=light&chartType=2&chartResolution=30&drawingToolbars=false"
         width="100%"
         height="100%"
         frameBorder="0"
