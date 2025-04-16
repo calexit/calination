c07920b479cde2b0de14c7abe0cadd53dccac697
0xsmiler <anthonylee110.eng@gmail.com>
2025-04-15 21:22:03 -0400

    modify the trading view

--- src/components/TradingView.jsx
+++ src/components/TradingView.jsx
@@ -4,10 +4,13 @@ export const TradingView = () => {
   return (
     <div className="w-full h-[500px]">
       <iframe
-        src="https://www.dextools.io/app/en/base/pair-explorer/0x086c01cd7891e8aed5fd27c01dcca6081b30318203aec3474c47464c030d9492?t=1744649619483"
+        src="https://www.dextools.io/app/en/base/pair-explorer/0x086c01cd7891e8aed5fd27c01dcca6081b30318203aec3474c47464c030d9492?embed=1&theme=light"
         width="100%"
         height="100%"
         title="CNT Trading Chart"
+        loading="lazy"
+        sandbox="allow-scripts allow-same-origin allow-popups"
+        referrerPolicy="no-referrer"
       ></iframe>
     </div>
   );
